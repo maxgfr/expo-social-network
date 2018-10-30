70107a77091167a4bea0f12386ff25ad7aa85cf5
Maxime Golfier <maxgolfier@gmail.com>
2018-10-30 18:09:59 +0100

    maxime_commit

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+
+import HomeScreen from '../screens/HomeScreen';
+import AddScreen from '../screens/AddScreen';
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  AddScreen: AddScreen
+});
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+  //Main: MainTabNavigator,
+  Main: HomeStack
 });
